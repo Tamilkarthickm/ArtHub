57551e445fe575e7c01cba49cea01a2a8c36683d
karthickt7 <thamizhkarthick07@gmail.com>
2025-03-16 12:03:11 +0530

    C++ Programs

---
+++ Inheritance.hpp
@@ -0,0 +1,30 @@
+class Base
+{
+private:
+int* ptr;
+public:
+    Base()
+    {
+        int data = 0;
+        ptr = &data;
+    }
+    ~Base()
+    {
+        ptr 
+    }
+};
+
+class Derived
+{
+public:
+    Derived()
+    {
+
+    };
+    ~Derived()
+    {
+
+    };
+};
+
+
