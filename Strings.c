c7141908c7ad7b2e3fb111c1b6ea397b4846f38d
karthickt7 <thamizhkarthick07@gmail.com>
2025-03-16 12:37:28 +0530

    String program

---
+++ Strings.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+/*
+-- Reading and Writing Strings
+-- Combining Strings Togeather
+-- Coping One String to Another
+-- Comparing Strings for Equality
+-- Extracing a portion of a string
+*/
+
+#define ARRAY_SIZE 10
+
+void main()
+{
+    char string_name[ARRAY_SIZE];
+    char city[] = "Bangalore";
+
+    printf("city = %s\n", city);
+    printf("string Name = %d\n", string_name);
+}
